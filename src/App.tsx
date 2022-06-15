87ba33b405ec910f779eb21d5cc2e047ce9ed8bf
Nikola Fimic <n.fimic@vegait.rs>
2022-06-15 15:34:36 +0200

    Added not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { Header } from './components/molecules/Header'
-import { Footer } from './components/molecules/Footer'
 import { HomePage } from './pages/HomePage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { NotFoundPage } from './pages/NotFoundPage'
+import { Layout } from './components/organism/Layout'
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path='/' element={<HomePage />}></Route>
-        <Route path='*' element={<h1> 404 NOT FOUND </h1>} />
+        <Route element={<Layout />}>
+          <Route path='/' element={<HomePage />} />
+        </Route>
+
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   )
 }
